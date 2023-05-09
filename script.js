e5ee86ed3df502d0ce794f7214ccfd9145d33769
Ismail Ali <ishaliprime@gmail.com>
2023-05-08 21:05:53 -0500

    set my 2 variables of fungus health and attack power to 100. Then I wrote code for all the click events and their functions. Then I wrote code for all the attack functions and what happens to the health bar and attack bar after and ran my render function

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ $(document).ready(onReady);
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+let fungusHP = 100;
+
+let attackPower = 100;
+
 
 function onReady() {
+    $('.arcane-scepter').on('click', attackScepterbtn);
+    $('.entangle').on('click', attackEntanglebtn);
+    $('.dragon-blade').on('click', attackDragonBladebtn);
+    $('.star-fire').on('click', attackStarFirebtn);
+
+    
+
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
@@ -16,3 +26,75 @@ function onReady() {
     // - Updates state which is ->
     // - Rendered to the DOM
 }
+
+function showOnDom() {
+    $('.ap-text').text(attackPower);
+    $('.hp-text').text(fungusHP);
+    fungusDead();
+
+
+}
+
+
+function attackScepterbtn(){
+    attackPower-=12;
+    fungusHP-=14
+    if(attackPower<=0){
+        attackPower=0
+        
+    };
+    if(fungusHP<=0){
+        fungusHP=0
+        
+    }
+    showOnDom()
+
+
+
+}
+function attackEntanglebtn(){
+   fungusHP-=9
+   attackPower-=23
+   if(attackPower<0){
+    attackPower=0
+   };
+   if(fungusHP<0){
+    fungusHP=0
+   }
+   showOnDom()
+}
+function attackDragonBladebtn(){
+    fungusHP-=47;
+    attackPower-=38
+    if(attackPower<0){
+        attackPower=0  
+    };
+    if(fungusHP<0){
+        fungusHP=0
+    }
+    showOnDom()
+}
+function attackStarFirebtn(){
+    fungusHP-=25;
+    attackPower-=33
+    if(attackPower<0){
+        attackPower=0
+    };
+    if(fungusHP<0){
+        fungusHP=0;
+    }
+    showOnDom();
+}
+
+function fungusDead(){
+
+
+
+
+
+// step 1. get buttons to console.log
+// step 2. get buttons to affect my ap
+// step 3. get buttons to affect fungus health
+// Last step. You may no longer attack, if AP is 0. 
+//Give all of the attack buttons a disabled attribute, 
+//so they may no longer be used.
